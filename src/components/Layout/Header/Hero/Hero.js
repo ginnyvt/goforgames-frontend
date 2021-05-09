77501396820db86e2fabb27b5b0f001f51a1160b
asus-ubuntu <ginny@powerkernel.com>
2021-05-09 23:40:51 +0300

    chore: make final restyles for the sit

--- src/components/Layout/Header/Hero/Hero.js
+++ src/components/Layout/Header/Hero/Hero.js
@@ -8,8 +8,7 @@ const Hero = () => {
       <div className='hero_banner'>
         {/* <h2 className='hero_title'>Exploring...</h2> */}
         <h3 className='hero_text'>
-          A place to meet new people, try new games, or play more of what you
-          love.
+          A place to meet new people, try new games, or play more what you love.
         </h3>
         {/* <Button className='btn-warning'>Get started</Button> */}
       </div>
